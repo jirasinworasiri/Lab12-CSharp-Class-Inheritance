078e791ee98bc436e9067535f807b9f1090c16ba
jirasinworasiri <aomjirasin457@gmail.com>
2025-03-29 22:22:24 +0700

    66030029 จิรสิน วรศิริ

---
+++ Lab12_Ex05/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class BaseClass1
+{
+    public BaseClass1()
+    {
+        System.Console.WriteLine("This is BaseClass1");
+    }
+}
+
+class BaseClass2
+{
+    public BaseClass2()
+    {
+        System.Console.WriteLine("This is BaseClass2");
+    }
+}
+
+// Interface ที่จะใช้สำหรับการเข้าถึงฟังก์ชันจาก BaseClass2
+interface IBaseClass2
+{
+    void MethodFromBaseClass2();
+}
+
+// DerivedClass สืบทอดจาก BaseClass1 และ Implement Interface IBaseClass2
+class DerivedClass : BaseClass1, IBaseClass2
+{
+    public DerivedClass() : base() // เรียก constructor ของ BaseClass1
+    {
+        System.Console.WriteLine("This is DerivedClass");
+    }
+
+    // Implement ฟังก์ชันจาก Interface
+    public void MethodFromBaseClass2()
+    {
+        System.Console.WriteLine("This method is from BaseClass2");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DerivedClass dc = new DerivedClass();
+        dc.MethodFromBaseClass2(); // เรียกใช้ method ที่มาจาก BaseClass2
+    }
+}
---
+++ Lab12_Ex09/Program.cs
@@ -0,0 +1,22 @@
+﻿DerivedClass dc = new DerivedClass();
+dc.Greeting();
+
+class BaseClass
+{
+    public string HelloBase = "Hello From BaseClass";  
+    public void Greeting()
+    {
+        Console.WriteLine("This is method in BaseClass");
+        Console.WriteLine(HelloBase);  
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    public string HelloDerived = "Hello From DerivedClass";  
+    public new void Greeting()  
+    {
+        Console.WriteLine("This is method in DerivedClass");
+        Console.WriteLine(HelloDerived);  
+    }
+}
